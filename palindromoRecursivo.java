3867a1f31c635ebdc983c678681cb89dcc058a32
isadoravrx <isavitoriaxavier@gmail.com>
2022-09-09 10:42:03 +0000

    programa de palindromo recursivo

--- palindromoRecursivo.java
+++ palindromoRecursivo.java
@@ -1,82 +1,25 @@
-import java.util.Scanner;
-public class palindromoRecursivo {
-
+public class palindromoRecursivo{
     public static void main(String[]args){
-        Scanner sc = new Scanner (System.in);
-        int number = sc.nextInt();
-        int quantifyOfNumbers = quantifyOfNumbers(number);
-        int []digits = new int[quantifyOfNumbers];
-        digits = digits(number,quantifyOfNumbers); 
-        int rightNumber = digits[0];
-        int leftNUmber = digits[digits.length -1];
-        int k = 0;
-        int l = -1;
-        boolean isPalindrome = isPalindrome(digits,rightNumber,leftNUmber,quantifyOfNumbers,k,l);
+        int [] number = {0, 2 , 3 , 2 , 0};
+        int rightIndex = 0;
+        int leftIndex = number.length - 1;
+        boolean isPalindrome = isPalindrome(number,rightIndex,leftIndex);
         if(isPalindrome == true){
-            System.out.println("this is a palindrome");
-        
+            System.out.println("é um palíndromo");
         }else{
-            System.out.println("this is not a palindrom");
-        } 
-
-   
-    
-    }public static int quantifyOfNumbers(int number){
-        int i = 0;
-        while( number > 0){
-            number = number / 10;
-            i++;
+            System.out.println("não é um palíndromo");
         }
-        return i;
+
+       
     }
 
-    public static int[] digits(int number, int quantifyOfNumbers){
-        int [] digits = new int[quantifyOfNumbers];
-        int digit;
-        for(int i = quantifyOfNumbers -1; i > -1; i--){
-            digit = number % 10;
-            digits[i] = digit;
-            number = number /10;
-        }
-        return digits;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    public static boolean isPalindrome(int [] digits, int rightNumber, int leftNumber,int quantifyOfNumbers, int k, int l){
-
-        if(quantifyOfNumbers % 2 ==0){
-            
-            if((l == quantifyOfNumbers / 2) && (rightNumber == leftNumber) ){//algo pra indicar que é o ultimo && (rightNumber == leftNumber)){
-                return true;
-            }
-            if(leftNumber == rightNumber){
-                
-                return isPalindrome(digits,digits[0+k],digits[quantifyOfNumbers + l],quantifyOfNumbers,k + 1, l - 1);
-            
-            }else{
-                return false;
-            }
-
-
-        }else{  
-            if(k == l){
-                return true;
-            }
-            if(leftNumber == rightNumber){
-                return isPalindrome(digits, digits[0+k], digits[quantifyOfNumbers - l], quantifyOfNumbers, k + 1, l + 1 );
-            }
-            else{
-                return false;
-            }
-
+    public static boolean isPalindrome(int [] number, int rightIndex, int leftIndex){
+        if(rightIndex >= leftIndex){
+            return true;
+        }if(number[rightIndex] == number[leftIndex]){
+            return isPalindrome(number, rightIndex + 1, leftIndex -1);
+        }else{
+            return false;
         }
     }
 }
-
- 
